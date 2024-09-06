d4be26ba491e13d938fb348d3d5d52ccdcfb91d7
Class Account <ee198-20-tab@eda-1.EECS.Berkeley.EDU>
2024-09-06 15:25:43 -0700

    fix paths + take out hammer mentor plugins since it doesn't get used, fix sram paths

--- scripts/init-vlsi.sh
+++ scripts/init-vlsi.sh
@@ -12,8 +12,8 @@ fi
 
 # Initialize HAMMER CAD-plugins
 if [[ $1 != *openroad* ]] && [[ $2 != *openroad* ]]; then
-    git submodule update --init --recursive vlsi/hammer-mentor-plugins
-    pip install -e vlsi/hammer-mentor-plugins
+	git submodule update --init --recursive vlsi/hammer
+	pip install -e vlsi/hammer
 fi
 
 # Initialize HAMMER tech plugin
