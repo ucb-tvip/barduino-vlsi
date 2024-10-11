022bb402078dc5d1047a5138febba18cfe9971f6
elamdf <elamdf@berkeley.edu>
2024-10-11 11:50:42 -0700

    looks like this at least prints, enables the core and then the core breaks

--- generators/chipyard/src/main/scala/config/OFOConfigs.scala
+++ generators/chipyard/src/main/scala/config/OFOConfigs.scala
@@ -10,23 +10,13 @@ import freechips.rocketchip.subsystem.{InCluster}
 class OFORocketConfig
     extends Config(
       new ofo.WithOFOCores(Seq(ofo.OneFiftyOneCoreParams())) ++
-  new chipyard.harness.WithDontTouchChipTopPorts(false) ++        // TODO FIX: Don't dontTouch the ports
-  new testchipip.soc.WithNoScratchpads ++                         // All memory is the Rocket TCMs
-  new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++ // use incoherent bus topology
-  new freechips.rocketchip.subsystem.WithNBanks(0) ++             // remove L2$
-  new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
-      new freechips.rocketchip.rocket.With1TinyCore ++                // single tiny rocket-core
-      //new chipyard.harness.WithSerialTLTiedOff ++ // don't attach anything to serial-tl
-        // new chipyard.config.WithBroadcastManager ++                 // Use broadcast-based coherence hub
-        /*
-             new testchipip.soc.WithNoScratchpads ++ // Remove subsystem scratchpads
+  new freechips.rocketchip.rocket.WithNHugeCores(1) ++                                  // single rocket-core
+  // TODO: make tinycore work, currently at least printing doesn't work persumably bc of the cache being a spad that TSI can't access (and thus can't print from)
 
-      //new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
-        new testchipip.soc.WithOffchipBusClient(freechips.rocketchip.subsystem.MBUS) ++ // off-chip bus connects to MBUS to provide backing memory
-        new chipyard.config.WithBroadcastManager ++                 // Use broadcast-based coherence hub
-        new testchipip.soc.WithOffchipBus ++ // Attach off-chip bus
-        new chipyard.config.WithBroadcastManager ++ // Replace L2 with a broadcast hub for coherence
-        */
-
+  // new chipyard.harness.WithDontTouchChipTopPorts(false) ++        // TODO FIX: Don't dontTouch the ports
+  // new testchipip.soc.WithNoScratchpads ++                         // All memory is the Rocket TCMs
+  // new freechips.rocketchip.subsystem.WithIncoherentBusTopology ++ // use incoherent bus topology
+  // new freechips.rocketchip.subsystem.WithNBanks(0) ++             // remove L2$
+  // new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
         new chipyard.config.AbstractConfig
     )
