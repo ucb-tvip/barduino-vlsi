212eac035abd33b02fbbc0ee1722e6be9c9b3d2f
elamdf <elamdf@berkeley.edu>
2024-10-11 22:33:40 -0700

    uh doesn't exactly work but what should be the bones of a working 2-core impl

---
+++ tests/build-for-ofo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ty abe for the reference -elam
+
+set -ex
+
+# $1 - name of specific binary to run on smartNIC core
+
+make
+
+BINARY_BUILD=${1}.riscv
+BINARY_OUTPUT=${1}.rocket.riscv
+
+# -O - output as a binary
+# --change-addresses - add N to the default address (aka DRAM) - move up 0x2000_0000 to 0xa000_0000
+    #-S
+    #-O binary \
+riscv64-unknown-elf-objcopy \
+    --change-addresses 0x20000000 \
+    $BINARY_BUILD \
+    $BINARY_OUTPUT
+
+# just verify that it can objdump the binary (valid bin)
+riscv64-unknown-elf-objdump -S $BINARY_OUTPUT >/dev/null
