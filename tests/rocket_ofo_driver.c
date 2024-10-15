86807100ff810c2ef2a77da21f46c515f34e9520
elamdf <elamdf@eecs.berkeley.edu>
2024-10-14 20:23:13 -0700

    at least this compiles, I think CLINT needs to be enabled for anything to work tho

--- tests/rocket_ofo_driver.c
+++ tests/rocket_ofo_driver.c
@@ -2,10 +2,9 @@
 
 #define RESET_REG 0x4000
 
-__attribute__((section(".secondary_text")))
 int main(void)
 {
-  printf("amogus\n");
+ // printf("amogus\n");
   // enable the core
   reg_write32(RESET_REG, 0);
   /*
