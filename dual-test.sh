58b827cbe5aca9b3802c862e1fa4f04b0860353e
elamdf <elamdf@eecs.berkeley.edu>
2024-10-14 20:23:13 -0700

    at least this compiles, I think CLINT needs to be enabled for anything to work tho

--- dual-test.sh
+++ dual-test.sh
@@ -10,6 +10,7 @@ RDIR=$(git rev-parse --show-toplevel)
 cd $RDIR
 
 DRIVER_BIN=rocket_ofo_driver
+DRIVER_BIN=accum
 OFO_BIN=write_a_lot
 
 pushd tests
@@ -34,9 +35,10 @@ rm -rf ${CFG}.out*
 # payload should load the 2nd binary (provided it has the right addresses)
 # # TODO undersatnd with soc1-msip is 
 # # +uartlog=${CFG}.out +write-soc1-msip +link_lat_a2s=10 +link_lat_s2a=10" 
+#
+    # USE_VPD=1\
 make CONFIG=${CFG} \
     BINARY=${SOC1_BIN} \
     EXTRA_SIM_FLAGS="+use-loadmem-hack +payload=${SOC2_BIN} +link_lat_a2s=10 +link_lat_s2a=10" \
-    USE_VPD=1\
     run-binary-debug
 rm -rf uartpty*
--- generators/chipyard/src/main/scala/config/OFOConfigs.scala
+++ generators/chipyard/src/main/scala/config/OFOConfigs.scala
@@ -2,7 +2,14 @@ package chipyard
 
 import org.chipsalliance.cde.config.{Config}
 import freechips.rocketchip.subsystem.{InCluster}
-import testchipip.boot.{WithBootAddrReg, BootAddrRegParams}
+import testchipip.boot.{BootAddrRegKey, BootAddrRegParams}
+
+import testchipip.serdes.{CanHavePeripheryTLSerial, SerialTLKey}
+import freechips.rocketchip.devices.tilelink.{CLINTParams, CLINTKey}
+
+
+
+import freechips.rocketchip.subsystem.{ExtBus, ExtMem, MemoryPortParams, MasterPortParams, SlavePortParams, MemoryBusKey}
 
 // --------------
 // OFO (EECS 151 ASIC) Core Configs
@@ -11,9 +18,9 @@ import testchipip.boot.{WithBootAddrReg, BootAddrRegParams}
 class OFORocketConfig
     extends Config(
       new ofo.WithOFOCores(Seq(ofo.OneFiftyOneCoreParams())) ++
+  new WithOFOSoCModifications ++ // enable dual memory regions, start the rocket at 0xa0000000
   new freechips.rocketchip.rocket.WithNHugeCores(1) ++                                  // single rocket-core
-  // the 151 core will start at 0x10000000
-  new WithBootAddrReg( BootAddrRegParams (defaultBootAddress = 0x20000000L)) ++ 
+
   // TODO: make tinycore work, currently at least printing doesn't work persumably bc of the cache being a spad that TSI can't access (and thus can't print from)
 
   // new chipyard.harness.WithDontTouchChipTopPorts(false) ++        // TODO FIX: Don't dontTouch the ports
@@ -23,3 +30,43 @@ class OFORocketConfig
   // new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
         new chipyard.config.AbstractConfig
     )
+class WithOFOSoCModifications extends Config(
+   new Config((site, here, up) => {
+    // disable tsi on this soc
+    //case SerialTLKey => None
+    // move CLINT to know addr
+    //case CLINTKey => Some(CLINTParams(baseAddress = BigInt("a0000000", 16)))
+    // have bootrom jump to proper dram loc
+    case BootAddrRegKey => up(BootAddrRegKey).map(_.copy(defaultBootAddress = BigInt("90000000", 16)))
+  }) ++
+  // setup memory to be at different location
+  new Config((site, here, up) => {
+    case ExtMem => Some(MemoryPortParams(MasterPortParams(
+      base = BigInt("80000000", 16),
+      size = BigInt("40000000", 16),
+      beatBytes = site(MemoryBusKey).beatBytes,
+      idBits = 7), // has to be 7 to match the app soc
+      1 // 1 mem. channels
+    ))
+  }) 
+/*
+  // setup slave port (slave to AppSoC)
+  new Config((site, here, up) => {
+    case ExtIn => Some(SlavePortParams(
+      beatBytes = 8, // 64b of data per xfer
+      idBits = 4, // 4b of source
+      sourceBits = 1 // ?? changes nothing
+    ))
+  }) ++
+  // setup master port (master to AppSoC)
+  new Config((site, here, up) => {
+    case ExtBus2 => Some(MasterPortParams(
+      base = 0x8000_0000,
+      size = 0x1000_0000,
+      beatBytes = site(MemoryBusKey).beatBytes, // 64b of data per xfer
+      idBits = 4, // 4b of source
+      executable = true // left true otherwise it will add extra bundle fields
+    ))
+  })
+*/
+)
