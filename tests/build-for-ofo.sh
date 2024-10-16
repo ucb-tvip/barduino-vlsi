97ad45681c665cec300418ba15d3827bc50d4ba2
elamdf <elamdf@eecs.berkeley.edu>
2024-10-15 18:57:32 -0700

    the 151 core at least boots, seems like it's not actually getting any instructions in response though

--- tests/build-for-ofo.sh
+++ tests/build-for-ofo.sh
@@ -9,14 +9,14 @@ set -ex
 make
 
 BINARY_BUILD=${1}.riscv
-BINARY_OUTPUT=${1}.rocket.riscv
+BINARY_OUTPUT=${1}.ofo.riscv
 
 # -O - output as a binary
 # --change-addresses - add N to the default address (aka DRAM) - move up 0x2000_0000 to 0xa000_0000
     #-S
     #-O binary \
 riscv64-unknown-elf-objcopy \
-    --change-addresses 0x10000000 \
+    --change-addresses 0x20000000 \
     $BINARY_BUILD \
     $BINARY_OUTPUT
 
