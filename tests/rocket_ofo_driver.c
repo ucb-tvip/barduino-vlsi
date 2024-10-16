c91d858f51ee48bb48d43d5b46889ce6fb99696a
elamdf <elamdf@eecs.berkeley.edu>
2024-10-15 18:57:32 -0700

    the 151 core at least boots, seems like it's not actually getting any instructions in response though

--- tests/rocket_ofo_driver.c
+++ tests/rocket_ofo_driver.c
@@ -4,12 +4,10 @@
 
 int main(void)
 {
- // printf("amogus\n");
+ printf("amogus\n");
   // enable the core
   reg_write32(RESET_REG, 0);
-  /*
   while(1) {
-    printf("amogus\n");
+    printf("waiting for ofo to crash...\n");
   }
-  */
 }
