4b52d14b27b7015227aa03df581a94fd01b74030
elamdf <elamdf@eecs.berkeley.edu>
2024-10-21 11:36:57 -0700

    fix driver code

--- tests/rocket_ofo_driver.c
+++ tests/rocket_ofo_driver.c
@@ -1,13 +1,22 @@
 #include "mmio.h"
 
 #define RESET_REG 0x4000
+#define CSR_REG 0x4004
 
 int main(void)
 {
- printf("amogus\n");
+  printf("enabling the 151t core...\n");
   // enable the core
   reg_write32(RESET_REG, 0);
-  while(1) {
-    printf("waiting for ofo to crash...\n");
+
+  int csr = reg_read32(CSR_REG);
+  while(csr == 0) {
+    csr = reg_read32(CSR_REG);
+  }
+
+  if (csr == 1) {
+    printf("pass!\n");
+  } else {
+    printf("fail :( CSR == %d\n", csr);
   }
 }
